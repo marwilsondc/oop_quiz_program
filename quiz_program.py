774fa59fe4e5289cb63bf911693c4819732c1dae
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-05-03 14:01:18 +0800

    added initial pseudocode for quiz program

---
+++ quiz_program.py
@@ -0,0 +1,14 @@
+#import pathlib and time
+#initialize file_path
+#define count_questions()
+#define get_question()
+#define get_choices()
+#define get_correct()
+#define check_ans()
+#initiate infinite while-loop
+#create a menu with different options and features:
+#show in the menu which file is currently opened
+#show in the menu how many questions are contained in the file
+#option: Ask random question; program asks a random question
+#option: Ask all questions; still random, but asks all questions
+#option: Quit program; breaks the loop and closes the program
