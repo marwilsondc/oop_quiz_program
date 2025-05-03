48f718fb818210d8f5cb19be97009f1417937154
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-05-03 14:30:25 +0800

    defined count_questions() for menu and randomizer range

--- quiz_program.py
+++ quiz_program.py
@@ -1,10 +1,33 @@
 #import pathlib and time
-#initialize file_path
-#define count_questions()
+from pathlib import Path
+import time
+
+#initialize file_path and file_exists
+file_path = Path("~", "Documents", "questions.txt").expanduser()
+file_exists: bool = None
+
+#program checks if questions.txt exists, if so, set file_exists to True. Otherwise, False
+if file_path.exists():
+    file_exists = True
+
+else:
+    print("Error: questions.txt does not exist")
+    time.sleep(1)
+    print("Create a question file first, then run this program")
+    file_exists = False
+
+#define count_questions(file_path: str)
+def count_questions():
+    global file_path
+
+    with open(file_path, "r") as file:
+        content = file.read()
+        return content.count("<question>")
 #define get_question()
 #define get_choices()
 #define get_correct()
 #define check_ans()
+#define change_dir(new_file: str)
 #initiate infinite while-loop
 #create a menu with different options and features:
 #show in the menu which file is currently opened
